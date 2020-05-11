65fea2b3c32c6cab4daa5b728bd334e09ad2ca58
Juanfran <49250701+rodavi02@users.noreply.github.com>
2020-05-11 17:07:40 +0200

    Delete firewall_db.sql

--- firewall_db.sql
+++
@@ -1,8 +0,0 @@
-USE firewall_db;
-CREATE TABLE access (
-user varchar(16) NOT NULL,
-password varchar(40) NOT NULL,
-CONSTRAINT user_pk PRIMARY KEY (user)
-);
-INSERT INTO access VALUES('juanfran',SHA('departamento'));
-INSERT INTO access VALUES('joseangel',SHA('iliberis'));
